0053ab2ce338432715c1c118b715d6b66eb1c988
pankratozzi <pankratozzi@gmail.com>
2022-09-14 23:29:11 +0300

    kaggle competition attempts

--- regression/sberbank_moscow_housing/utils.py
+++ regression/sberbank_moscow_housing/utils.py
@@ -23,6 +23,12 @@ from lightgbm import LGBMClassifier, LGBMRegressor
 import catboost
 from catboost import CatBoostRegressor, Pool
 import xgboost as xgb
+import h3
+from geojson import Feature, Point, FeatureCollection
+import json
+import matplotlib
+import folium
+from folium import Choropleth, Circle, Marker
 
 
 seed = 42
@@ -691,3 +697,61 @@ def evaluate_preds(true_values, pred_values, save=False):
         plt.savefig('./saved/' + 'report.png')
     plt.tight_layout()
     plt.show()
+
+
+def hexagons_dataframe_to_geojson(df_hex, file_output=None, column_name="value"):
+    """
+    Produce the GeoJSON for a dataframe, constructing the geometry from the "hex_id" column
+    and with a property matching the one in column_name
+    """
+    list_features = []
+
+    for i, row in df_hex.iterrows():
+        try:
+            geometry_for_row = {"type": "Polygon", "coordinates": [h3.h3_to_geo_boundary(h=row["hex"], geo_json=True)]}
+            feature = Feature(geometry=geometry_for_row, id=row["hex"], properties={column_name: row[column_name]})
+            list_features.append(feature)
+        except:
+            print("An exception occurred for hex " + row["hex"])
+
+    feat_collection = FeatureCollection(list_features)
+    geojson_result = json.dumps(feat_collection)
+    return geojson_result
+
+
+def get_color(custom_cm, val, vmin, vmax):
+    return matplotlib.colors.to_hex(custom_cm((val - vmin) / (vmax - vmin)))
+
+
+def choropleth_map(df_aggreg, column_name="value", border_color='black', fill_opacity=0.7, color_map_name="Blues",
+                   initial_map=None):
+    """
+    Creates choropleth maps given the aggregated data. initial_map can be an existing map to draw on top of.
+    """
+    min_value = df_aggreg[column_name].min()
+    max_value = df_aggreg[column_name].max()
+    mean_value = df_aggreg[column_name].mean()
+    print(f"Colour column min value {min_value}, max value {max_value}, mean value {mean_value}")
+    print(f"Hexagon cell count: {df_aggreg['hex'].nunique()}")
+
+    name_layer = "Choropleth " + str(df_aggreg)
+
+    if initial_map is None:
+        initial_map = folium.Map(location=[47, 4], zoom_start=5.5, tiles="cartodbpositron")
+
+    geojson_data = hexagons_dataframe_to_geojson(df_hex=df_aggreg, column_name=column_name)
+
+    custom_cm = matplotlib.cm.get_cmap(color_map_name)
+
+    folium.GeoJson(
+        geojson_data,
+        style_function=lambda feature: {
+            'fillColor': get_color(custom_cm, feature['properties'][column_name], vmin=min_value, vmax=max_value),
+            'color': border_color,
+            'weight': 1,
+            'fillOpacity': fill_opacity
+        },
+        name=name_layer
+    ).add_to(initial_map)
+
+    return initial_map
